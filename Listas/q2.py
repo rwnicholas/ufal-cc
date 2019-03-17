74bf54b847adaf9430001366fe4bf2784d7c9069
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2019-03-17 20:37:42 -0300

    Selection sort

--- Listas/q2.py
+++ Listas/q2.py
@@ -2,23 +2,26 @@
 
 from lista import Lista
 principal = Lista()
-agenda = [('nick', 999), ('lucas', 444),('dudu', 666), ('gean', 616)]
+agenda = [('nick', 999), ('lucas', 444), ('fillipe', 555),('dudu', 666), ('gean', 616)]
 def reorganizar(item):
 	principal.inserir(item)
 	no = principal.inicio.proximo
 	while no != None:
 		no2 = principal.inicio.proximo
+		menor = no.item
 		while no2 != None:
-			if no != no2 and no.item < no2.item:
-				index1 = principal.buscar(no.item)
-				principal.remover(index1)
-				principal.inserir(no2.item, index1)
-
-				index2 = principal.buscar(no2.item)
-				principal.remover(index2)
-				principal.inserir(no.item, index2)
-
+			if no != no2 and no.item[0] > no2.item[0]:
+				menor = no2.item
+		
 			no2 = no2.proximo
+
+		index2 = principal.buscar(menor)
+		principal.remover(index2)
+		principal.inserir(no.item, index2)
+
+		index1 = principal.buscar(no.item)
+		principal.remover(index1)
+		principal.inserir(menor, index1)
 		no = no.proximo
 
 for x in agenda:
